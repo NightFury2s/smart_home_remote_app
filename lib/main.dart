17ce92778243dd5596508406ef84b27687472e85
NightFury <89623168+NightFury2s@users.noreply.github.com>
2024-04-04 20:38:37 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:IntelliHome/screen/Devices/devices.dart';
 import 'package:IntelliHome/screen/SettingPage/setting_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,7 @@ import 'package:IntelliHome/constants/app_colors.dart';
 import 'package:IntelliHome/screen/Auth/Login/login_page.dart';
 import 'package:IntelliHome/screen/Auth/Register/register_page.dart';
 import 'package:IntelliHome/screen/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:IntelliHome/dbHelper/mongodb.dart';
 import 'package:IntelliHome/screen/splash_home.dart';
 import 'firebase_options.dart';
@@ -37,8 +39,43 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginPage(),
         '/signup': (context) => RegisterPage(),
         '/setting': (context) => SettingPage(),
+        '/devices': (context) => Devices(),
       },
-      home: SplashHome(title: 'Smart Home App'),
+      home: const InitializerWidget(),
     );
+  }
+}
+
+class InitializerWidget extends StatefulWidget {
+  const InitializerWidget({Key? key}) : super(key: key);
+
+  @override
+  _InitializerWidgetState createState() => _InitializerWidgetState();
+}
+
+class _InitializerWidgetState extends State<InitializerWidget> {
+  @override
+  void initState() {
+    super.initState();
+    navigateUser(); // Call navigateUser() when widget is created
+  }
+
+  navigateUser() async {
+    // Wait for 3 seconds before navigating to the next page
+    await Future.delayed(Duration(seconds: 3));
+    // Check if the user is logged in
+    if (FirebaseAuth.instance.currentUser != null) {
+      // If the user is logged in, navigate to the next page
+      Navigator.of(context).pushReplacementNamed('/home');
+    } else {
+      // User is not logged in, navigate to the login page
+      Navigator.of(context).pushReplacementNamed('/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Back to home screen after 3 seconds
+    return const SplashHome(title: 'Smart Home App');
   }
 }
